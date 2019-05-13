45f46c732ccd69fb0ae3db9a736c9578c6f83267
Vincent Giersch <vincent@giersch.fr>
2019-05-13 18:37:18 +0200

    chore(build): 1.0.0

--- dist/embed.js
+++ dist/embed.js
@@ -1,4 +1,4 @@
-/*! flat-embed v1.0.0-rc0 | (c) 2019 Tutteo Ltd. (Flat) | Apache-2.0 License | https://github.com/FlatIO/embed-client */
+/*! flat-embed v1.0.0 | (c) 2019 Tutteo Ltd. (Flat) | Apache-2.0 License | https://github.com/FlatIO/embed-client */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
